c6e4f0ec724ba1b9469c0e41339095d608a1677f
Praise <praisetoyo@gmail.com>
2024-05-04 22:02:43 +0100

    feat: edit and delete urls

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import { createShortUrls, getAllUrls, getUrl } from '../controllers/url';
+import { createShortUrls, getAllUrls, getUrl, editUrl, deleteUrl } from '../controllers/url';
 import { validateUrl, validateId, validateRequest } from '../middleware/validate-url';
+import validateEditUrl from '../middleware/validate-edit-urls';
 
 const router = express.Router();
 
 router.post('/shorten', validateUrl, validateRequest, createShortUrls);
 router.get('/urls', getAllUrls);
 router.get('/urls/:id', validateId, validateRequest, getUrl);
+router.put('/urls/:id', validateId, validateEditUrl, validateRequest, editUrl);
+router.delete('/urls/:id', validateId, validateRequest, deleteUrl);
 
 export default router;
