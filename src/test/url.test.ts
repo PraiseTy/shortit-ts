809b6e1064e7115e254ccff33ab42b2ea1a0f6d9
John Oshalusi <john.oshalusi@iohk.io>
2024-05-16 21:07:39 +0100

    test: add tests for get route

--- src/test/url.test.ts
+++ src/test/url.test.ts
@@ -12,8 +12,44 @@ describe('Shortener', () => {
       factory.close().then(done);
     });
 
-    it.todo('should return an empty array of urls');
+    it('should return an empty array of urls', async () => {
+      const response = await factory.app.get('/urls');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual([]);
+    });
 
-    it.todo('should return an array of urls');
+    it('should return an array of urls', async () => {
+      const res1 = await factory.app.post('/shorten').send({ url: 'https://www.google.com' });
+      expect(res1.status).toBe(201);
+      const response = await factory.app.get('/urls');
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(1);
+      expect(response.body[0].originalUrl).toBe('https://www.google.com');
+    });
+  });
+
+  describe('POST /urls', () => {
+    it.todo('should save url and return a new shortened url');
+    it.todo('should return an error if the request body is invalid');
+    it.todo('should return the right message if original url already exists');
+    it.todo('should return an error if custom name already exists');
+  });
+
+  describe('GET /urls/:id', () => {
+    it.todo('should return a single url');
+    it.todo('should return an error if the url does not exist');
+  });
+
+  describe('PUT /urls/:id', () => {
+    it.todo('should update a single url');
+    it.todo('should return an error if the url id does not exist');
+    it.todo('should return an error if the request body is invalid');
+    it.todo('should return an error if the custom name already exists');
+  });
+
+  describe('DELETE /urls/:id', () => {
+    it.todo('should delete a single url');
+    it.todo('should return an error if the url id does not exist');
+    it.todo('should return an error if the url id is invalid');
   });
 });
