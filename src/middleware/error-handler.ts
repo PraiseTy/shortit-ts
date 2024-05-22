e4a1b2b7e17558278771c8fdd788d63f34b02f0e
Praise <praisetoyo@gmail.com>
2024-05-22 13:36:22 +0100

    test: change error statements

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -6,10 +6,10 @@ import { HTTP_ERRORS } from '../utils';
 // eslint-disable-next-line consistent-return, @typescript-eslint/no-unused-vars
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof CustomError) {
-    return res.status(error.statusCode).json({ errors: error.serializeErrors() });
+    return res.status(error.statusCode).send({ errors: error.serializeErrors() });
   }
 
   logger.error(error.message);
-  res.status(HTTP_ERRORS.INTERNAL_SERVER_ERROR).json({ errors: [{ message: 'Something went wrong' }] });
+  res.status(HTTP_ERRORS.INTERNAL_SERVER_ERROR).send({ errors: [{ message: 'Something went wrong' }] });
   // next();
 };
